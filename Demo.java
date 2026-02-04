dd099a89cc927e3f46ee640aae26de8afee34bdc
RudraRaushan01 <rudraraushan000@gmail.com>
2026-02-04 21:47:32 +0530

    Second commit

--- Demo.java
+++ Demo.java
@@ -2,4 +2,8 @@ public class Demo{
     public static void main(String[] args) {
         System.out.println("Hello, World!");
     }
+
+    void show(){
+        System.out.println("Demo class method");
+    }
 }
